747ff65c81e56c4a8272987dcafa91005ee3efe3
Guðmundur Bjarki Sigurðsson <gudmundur.bjarki.sigurdsson@ruv.is>
2024-03-29 15:33:39 +0000

    split up init.lua

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,7 +1,7 @@
-return {                -- Useful plugin to show you pending keybinds.
+return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
-  event = 'VimEnter',   -- Sets the loading event to 'VimEnter'
-  config = function()   -- This is the function that runs, AFTER loading
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
     require('which-key').setup()
 
     -- Document existing key chains
